f0d77ae18d3ef8fa7b6a32975dd22a02bed5561b
Tomasz <haboomba11@gmail.com>
2024-02-11 20:33:09 +0100

    sec 11_02

--- Training/javascript/static/main.js
+++ Training/javascript/static/main.js
@@ -1,1 +1,26 @@
- console.log('test')
+console.log('test')
+
+let carMileage
+carMileage = 2000000
+if (carMileage < 0) {
+    console.log(`blad licznika ${carMileage}`)
+}
+if (carMileage === 0) {
+    console.log('nowe autro')
+}
+if (carMileage <= 100) {
+    console.log(`prawie nowe auto. licznik: ${carMileage}`)
+}
+if (carMileage < 10000) {
+    console.log('auto wymagahace pierwszego przegsladu')
+}
+if (carMileage <= 200000) {
+    console.log(`auto wymagajace dokładneog sprawdzenia`)
+}
+if (carMileage >= 1000000) {
+    console.log('auto do remontu')
+}
+
+let obj = {czlowiek: 'Tomasz'}
+console.log(obj.czlowiek)
+
