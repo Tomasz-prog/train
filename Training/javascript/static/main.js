cce7981f9991b3d2091b65030446fa402360d9d3
Tomasz <haboomba11@gmail.com>
2024-02-13 20:55:26 +0100

    sec 13_02

--- Training/javascript/static/main.js
+++ Training/javascript/static/main.js
@@ -1,27 +1,11 @@
-console.log('test')
 
-let carMileage
-carMileage = 2000000
-if (carMileage < 0) {
-    console.log(`blad licznika ${carMileage}`)
-}
-if (carMileage === 0) {
-    console.log('nowe autro')
-}
-if (carMileage <= 100) {
-    console.log(`prawie nowe auto. licznik: ${carMileage}`)
-}
-if (carMileage < 10000) {
-    console.log('auto wymagahace pierwszego przegsladu')
-}
-if (carMileage <= 200000) {
-    console.log(`auto wymagajace dokładneog sprawdzenia`)
-}
-if (carMileage >= 1000000) {
-    console.log('auto do remontu')
+
+let name = "Kasia"
+
+if ( typeof name == "string") {
+    let name = "Ola"
+    console.log(name)
+
 }
 
-let obj = {czlowiek: 'Tomasz'}
-console.log(obj.czlowiek)
-
-if (true && true) console.log('#1');
+console.log(name)
